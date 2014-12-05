3495ea890ad1ac11bfca8c15f28a66e8fa3a5b16
Vraj Pandya <vrajspandya@gmail.com>
2014-12-05 14:37:24 +0530

    MasterPage is ready; removed older ones

--- HMS/HMSWeb/MasterPage.master.cs
+++ HMS/HMSWeb/MasterPage.master.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BusinessLogic;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        AppUser LoggedAppUser = (AppUser)Session["AppUser"];
 
+        if (LoggedAppUser == null)
+        {
+            Response.Redirect("login.aspx");
+        }
+        else
+        {
+            lblUsername.Text = LoggedAppUser.Name;
+        }
+    }
+    protected void btnLogout_Click(object sender, EventArgs e)
+    {
+        AppUser LoggedAppUser = (AppUser)Session["AppUser"];
+        Session["AppUser"] = null;
+        Response.Redirect("login.aspx");
     }
 }
